20e72d0d404c31d1b01acde85267139058ca5bb9
Ciera Garrett <cngarrett@macbook-air.attlocal.net>
2022-11-30 15:07:29 -0500

    modify return string to be JSON

--- app.py
+++ app.py
@@ -15,9 +15,8 @@ def balance(address):
     url = f"https://mainnet.infura.io/v3/{os.environ['API_KEY']}"
     myobj = {"jsonrpc":"2.0","method":"eth_getBalance","params": [address, "latest"],"id":1}
     x = requests.post(url, json = myobj)
-    return str(
-        float.fromhex(
-            json.loads(x.text)['result']
-        )/1000000000000000000 # wei to eth conversion
-    )
+    balance = str(float.fromhex(json.loads(x.text)['result'])/1000000000000000000) # wei to eth conversion
+    return json.dumps({'balance': balance})
+        
+    
     
